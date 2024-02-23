dd41bc52df4ca7fc03477c56a0d59e3fbadde60a
Steven Christian Susanto <nicenicenice345@gmail.com>
2024-02-23 12:50:15 +0700

    add: routes1

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/hello', function () {
+    return 'Hello World';
+});
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/world', function () {
+    return 'World';
+});
+   
+Route::get('/welcome', function() {
+    return 'Welcome to my website!';
+});
+
+Route::get('/about', function() {
+    return 'NIM:224172xxxx <br/> Name: Steven';
 });
